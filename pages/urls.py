738a71d4f8047282338717b923e2f7f59918e8e4
teweger <teweger@ucsc.edu>
2023-10-09 19:29:13 -0700

    quick margin change

--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from pages import views
 
 urlpatterns = [
+    path('download_pdf/<str:p>/', views.download_pdf, name="download_pdf"),
     path("", views.home, name='home'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,23 @@
+import os
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'pages/home.html', {})
+    return render(request, "pages/home.html",{})
+
+def download_pdf(request, *args, **kwargs):
+    # Get the filename from the URL parameters
+    filename = kwargs.get('p')
+
+    # Define the path to the PDF file
+    file_path = os.path.join('static', 'pdfs', filename)
+
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as fh:
+            response = HttpResponse(fh.read(), content_type="application/pdf")
+            response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
+            return response
+    else:
+        raise Http404("PDF file not found")
